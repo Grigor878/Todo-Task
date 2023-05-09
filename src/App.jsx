f977e79db3020bee434c6f143f26a83dd9d4d0be
Grigor878 <grigsgog@gmail.com>
2023-05-09 21:59:24 +0400

    task2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import "./App.scss";
-import TodoFrom from './components/todo/TodoFrom';
-import TodoItem from './components/todo/TodoItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import TodoList from './components/todo/TodoList';
+import TodoFrom from './components/todo/TodoFrom';
+import { addTodo, deleteTodo, updateTodo, toggleCompleted } from './components/store/store';
+import './App.scss';
 
 function App() {
+  const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos);
+
+  useEffect(() => {
+    // You can use this useEffect hook to fetch data from a server or do other initialization logic
+  }, []);
+
+  const handleAddTodo = ({ title, description, deadline }) => {
+    dispatch(addTodo({ title, description, deadline }));
+  };
+
+  const handleToggleCompleted = id => {
+    dispatch(toggleCompleted(id));
+  };
+
+  const handleEdit = (id, { title, description, deadline }) => {
+    dispatch(updateTodo({ id, title, description, deadline }));
+  };
+
+  const handleDelete = id => {
+    dispatch(deleteTodo(id));
+  };
+
   return (
-    <>
-      <TodoFrom />
-      <TodoItem />
-      <TodoList />
-    </>
+    <div className="app">
+      <h1>Todo List</h1>
+      <TodoFrom onAddTodo={handleAddTodo} />
+      <TodoList
+        todos={todos}
+        onToggleCompleted={handleToggleCompleted}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+    </div>
   );
 }
 
